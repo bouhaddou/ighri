4f92aec210d4377683a8e5fa459286398ea246b4
bouhaddou <brahimbouhaddou12@gmail.com>
2020-01-30 14:38:49 +0100

    add les fausse donne

--- src/Controller/EcommerceController.php
+++ src/Controller/EcommerceController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
+use App\Repository\ProduitsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+    /**
+     * @Route("/produits")
+     */
 class EcommerceController extends AbstractController
 {
     /**
-     * @Route("/produits", name="produitspage")
+     * @Route("/", name="produitspage")
      */
     public function index()
     {
-        return $this->render('produits/index.html.twig', [ ]);
+        return $this->render('produits/pages/index.html.twig', [ ]);
     }
     
+    /**
+     * @Route("/All", name="AllProduits")
+     */
+    public function details(CategorieRepository $categorie)
+    {
+        return $this->render('produits/pages/produits.html.twig', [ 
+            'categories' => $categorie->findAll(),
+        ]);
+    }
+    
+
     /**
      * @Route("/ecommerce/festival", name="festivalpage")
      */
