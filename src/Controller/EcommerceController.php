2442a3d1c922e46190992b0208fb4de3787a43a1
bouhaddou <brahimbouhaddou12@gmail.com>
2020-01-31 00:11:07 +0100

    add cart

--- src/Controller/EcommerceController.php
+++ src/Controller/EcommerceController.php
@@ -11,17 +11,25 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+ $php2js = new Produits();
+  
+   
+    
     /**
      * @Route("/produits")
      */
 class EcommerceController extends AbstractController
 {
+     
+
     /**
      * @Route("/", name="produitspage")
      */
-    public function index()
+    public function index(CategorieRepository $categorie)
     {
-        return $this->render('produits/pages/index.html.twig', [ ]);
+        return $this->render('produits/pages/index.html.twig', [ 
+            'categories' => $categorie->findAll(),
+        ]);
     }
     
     /**
@@ -31,24 +39,48 @@ class EcommerceController extends AbstractController
     { 
         $pagination->setEntityClass(Produits::class)
                     ->setPage($page)
-                    ->setLimit(30);
+                    ->setLimit(20);
 
         return $this->render('produits/pages/produits.html.twig', [ 
             'categories' => $categorie->findAll(),
-            'pagination' => $pagination
+            'produits' => $pagination->getData()
         ]);
     }
     
     /**
      * @Route("/details/{id}", name="DetailsProduits")
      */
-    public function DetailsProduits(ProduitsRepository $produit,$id,ObjectManager $manger,Request $request)
+    public function DetailsProduits(ProduitsRepository $produit,$id,CategorieRepository $categorie)
     { 
         return $this->render('produits/pages/produit-single.html.twig', [ 
-            'produit' => $produit->findOneBy([ 'id' => $id])
-            
+            'produit' => $produit->findOneBy([ 'id' => $id]),
+            'categories' => $categorie->findAll(),
         ]);
     }
 
+     
+    /**
+     * @Route("/Categorie/{id}/produit", name="SearcheProduits")
+     */
+    public function SearchByCategorie(ProduitsRepository $produits,$id,CategorieRepository $categorie)
+    { 
+        $result=$categorie->findOneBy([ 'id' => $id]);
+            dd($produits->findProduitByCategorie( $result));
+        return $this->render('produits/pages/produitParCategorie.html.twig', [ 
+            'categories' => $categorie->findAll(),
+            'produits' => $produits->findProduitByCategorie( $result)
+        ]);
+    }
+
+     /**
+     * @Route("/cart/{id}/produit", name="CartProduits")
+     */
+    public function CartProduit(ProduitsRepository $produits,$id,ObjectManager $manager)
+    { 
+        
+        $php2js=$produits->findOneBy([ 'id' => $id]);
+        $manager->persist($php2js);
+        return $this->redirectToRoute('dd');
+    }
     
 }
--- src/Repository/ProduitsRepository.php
+++ src/Repository/ProduitsRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Categorie;
 use App\Entity\Produits;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
@@ -19,22 +20,20 @@ class ProduitsRepository extends ServiceEntityRepository
         parent::__construct($registry, Produits::class);
     }
 
-    // /**
-    //  * @return Produits[] Returns an array of Produits objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Produits[] Returns an array of Produits objects
+     */
+    
+    public function findProduitByCategorie(Categorie $categorie)
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
+                    ->andWhere('p.categories = :val')
+                    ->setParameter('val', $categorie)
+                    ->getQuery()
+                    ->getResult()
         ;
     }
-    */
+    
 
     /*
     public function findOneBySomeField($value): ?Produits
